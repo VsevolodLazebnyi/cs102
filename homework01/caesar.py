4b0472f9c8f3b3819f78ebaff2f6cefd74637dd1
VsevolodLazebnyi <vllazebnyi@gmail.com>
2022-11-30 19:21:56 +0300

    Реализована функция encrypt_caesar()

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -1,6 +1,7 @@
 def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     """
     Encrypts plaintext using a Caesar cipher.
+
     >>> encrypt_caesar("PYTHON")
     'SBWKRQ'
     >>> encrypt_caesar("python")
@@ -11,13 +12,48 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    Alpha_Big = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                 'U', 'V', 'W', 'X', 'Y', 'Z']
+    Alpha_small = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                   'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                   'u', 'v', 'w', 'x', 'y', 'z']
+
+    word_list = list(" ".join(map(str, (plaintext.split()))))
+    new_word = []
+    for i in range(0, len(word_list)):
+        if word_list[i] in Alpha_small:
+            n = Alpha_small.index(word_list[i]) + shift
+            while n >= len(Alpha_small) - 1:
+                n = n - len(Alpha_small)
+            while n <= -(len(Alpha_small)):
+                n = n + len(Alpha_small) - 1
+            else:
+                pass
+            new_word.append(Alpha_small[n])
+        elif word_list[i] in Alpha_Big:
+            n = Alpha_Big.index(word_list[i]) + shift
+            while n >= len(Alpha_small) - 1:
+                n = n - len(Alpha_small)
+            while n <= -(len(Alpha_small)):
+                n = n + len(Alpha_small) - 1
+            else:
+                pass
+            new_word.append(Alpha_Big[n])
+        else:
+            new_word.append(word_list[i])
+    word_str = ''
+    for j in range(0, len(new_word)):
+        word_str = word_str + str(new_word[j])
+    ciphertext = word_str
     return ciphertext
 
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
     Decrypts a ciphertext using a Caesar cipher.
+
     >>> decrypt_caesar("SBWKRQ")
     'PYTHON'
     >>> decrypt_caesar("sbwkrq")
@@ -28,5 +64,40 @@ def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     ''
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    Alpha_Big = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                 'U', 'V', 'W', 'X', 'Y', 'Z']
+    Alpha_small = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+                   'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                   'u', 'v', 'w', 'x', 'y', 'z']
+
+    word_list = list(" ".join(map(str, (ciphertext.split()))))
+    new_word = []
+    for i in range(0, len(word_list)):
+        if word_list[i] in Alpha_small:
+            n = Alpha_small.index(word_list[i]) - shift
+            while n >= len(Alpha_small) - 1:
+                n = n - len(Alpha_small)
+
+            while n <= -(len(Alpha_small)):
+                n = n + len(Alpha_small) - 1
+            else:
+                pass
+            new_word.append(Alpha_small[n])
+        elif word_list[i] in Alpha_Big:
+            n = Alpha_Big.index(word_list[i]) - shift
+            while n >= len(Alpha_small) - 1:
+                n = n - len(Alpha_small)
+            while n <= -(len(Alpha_small)):
+                n = n + len(Alpha_small) - 1
+            else:
+                pass
+            new_word.append(Alpha_Big[n])
+        else:
+            new_word.append(word_list[i])
+    word_str = ''
+    for j in range(0, len(new_word)):
+        word_str = word_str + str(new_word[j])
+    plaintext = word_str
     return plaintext
