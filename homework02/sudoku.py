9411a055a956de693c20305341d7ce507f47710c
VsevolodLazebnyi <vllazebnyi@gmail.com>
2022-12-14 17:47:49 +0300

    Реализована функция group()

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -41,7 +41,7 @@ def group(values: tp.List[T], n: int) -> tp.List[tp.List[T]]:
     >>> group([1,2,3,4,5,6,7,8,9], 3)
     [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     """
-    pass
+    return [values[i:i + n] for i in range(0, len(values), n)]
 
 
 def get_row(grid: tp.List[tp.List[str]], pos: tp.Tuple[int, int]) -> tp.List[str]:
