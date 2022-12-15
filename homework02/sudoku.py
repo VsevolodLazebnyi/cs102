f94c33f98df35f31f3924e6a40ce11e644900c7a
VsevolodLazebnyi <vllazebnyi@gmail.com>
2022-12-15 11:26:28 +0300

    Исправлены ошибки

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -121,11 +121,11 @@ def solve(grid: tp.List[tp.List[str]]) -> tp.Optional[tp.List[tp.List[str]]]:
     >>> solve(grid)
     [['5', '3', '4', '6', '7', '8', '9', '1', '2'], ['6', '7', '2', '1', '9', '5', '3', '4', '8'], ['1', '9', '8', '3', '4', '2', '5', '6', '7'], ['8', '5', '9', '7', '6', '1', '4', '2', '3'], ['4', '2', '6', '8', '5', '3', '7', '9', '1'], ['7', '1', '3', '9', '2', '4', '8', '5', '6'], ['9', '6', '1', '5', '3', '7', '2', '8', '4'], ['2', '8', '7', '4', '1', '9', '6', '3', '5'], ['3', '4', '5', '2', '8', '6', '1', '7', '9']]
     """
-    a = find_empty_positions(grid)
-    if not a:
+    empt = find_empty_positions(grid)
+    if empt is None:
         return grid
-    row, col = a
-    for n in find_possible_values(grid, a):
+    row, col = empt
+    for n in find_possible_values(grid, empt):
         grid[row][col] = n
         if solve(grid):
             s = solve(grid)
