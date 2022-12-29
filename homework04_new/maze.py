04ecd62bbfbd284c018327390330642386474c72
VsevolodLazebnyi <vllazebnyi@gmail.com>
2022-12-29 07:26:22 +0300

    исправил импорты во всех файлах

--- homework04_new/maze.py
+++ homework04_new/maze.py
@@ -1,3 +1,10 @@
+from copy import deepcopy
+from random import choice, randint
+from typing import List, Optional, Tuple, Union
+
+import pandas as pd  # type: ignore
+
+
 def create_grid(rows: int = 15, cols: int = 15) -> List[List[Union[str, int]]]:
     return [["■"] * cols for _ in range(rows)]
 
